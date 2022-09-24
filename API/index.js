e59e1add2d3211ac430f9eebf3e7238b4159725c
Bennett Davis <bbdavis3@crimson.ua.edu>
2022-09-23 23:35:57 -0500

    Created MongoDB Database and Product Schema

--- API/index.js
+++ API/index.js
@@ -1,7 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import {Product} from './Schemas/ProductSchema.js'
 
 const app = express();
 
-app.listen(3001, () =>
-  console.log('Example app listening on port 3000!'),
-);
+try {
+  await mongoose.connect('mongodb://127.0.0.1:27017/product_database');
+
+  app.listen(3001, () =>
+    console.log('Example app listening on port 3000!'),
+  );
+} 
+catch(error) {
+  console.error(error);
+  process.exit(1);
+}
+
