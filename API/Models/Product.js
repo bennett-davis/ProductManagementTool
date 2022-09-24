61a2c9506f95701f7e510ce15a9580869e99567e
Bennett Davis <bbdavis3@crimson.ua.edu>
2022-09-23 21:39:10 -0500

    Created API Models

---
+++ API/Models/Product.js
@@ -0,0 +1,12 @@
+
+class Product {
+
+    constructor(name, upc, available_on) {
+        this.name = name;
+        this.upc = upc;
+        this.available_on = available_on;
+    }
+
+}
+
+module.exports = Product; 
